feb5324dc7e588617430594a4e0cf12640ac3f0b
Badrelddin <badrhamidou@gmail.com>
2023-05-21 20:40:57 +0200

    re-design DB

--- bbdd.sql
+++ bbdd.sql
@@ -17,32 +17,22 @@ create table empleado(
     fecha_nacimiento date
 );
 
-create table test(
-	id int auto_increment primary key, 
-    tipo varchar(25)
-);
 
 create table testEquipo(
 	id int auto_increment primary key, 
-    id_test int,
-    FOREIGN KEY (id_test) references test(id) on delete cascade,
     respuesta1 varchar(25),
-	respuesta2 varchar(25),
+	id_empleado varchar(25),
     sobra varchar(25) default "Nadie"
 );
 
 create table testLider(
 	id int auto_increment primary key, 
-        id_test int,
-    FOREIGN KEY (id_test) references test(id) on delete cascade,
     evalua float
 );
 
 create table testEstadoAnimico(
 	id int auto_increment primary key, 
-        id_test int,
-            id_empleado int,
-    FOREIGN KEY (id_test) references test(id) on delete cascade,
+	id_empleado int,
 	FOREIGN KEY (id_empleado) references empleado(id) on delete cascade,
 	
     sentimiento1 varchar(25),
@@ -51,6 +41,4 @@ create table testEstadoAnimico(
     sentimiento4 varchar(25),
     fecha_realizado datetime
 );
-
-
 
